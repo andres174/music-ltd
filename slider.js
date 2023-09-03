65c93e22cafd115dc4db493ebad8395be94ebecb
andres174 <zambrasda17@gmail.com>
2023-09-03 09:30:27 -0500

    slide con js puro

--- slider.js
+++ slider.js
@@ -4,6 +4,8 @@ const slides = slider.querySelectorAll(".slide");
 const controls = document.querySelector(".controls");
 const controlsElements = controls.querySelectorAll(".control");
 
+const loader = document.querySelector(".loader");
+
 let currentSlide = 0;
 
 function nextSlide() {
@@ -23,9 +25,17 @@ function nextSlide() {
 
     controlsElements[currentSlide].classList.remove("inactive");
     controlsElements[currentSlide].classList.add("active");
+
+    loader.style.display = "none";
 }
 
-setInterval(nextSlide, 3000);
+setInterval(nextSlide, 2000);
+
+slides.forEach((slide, index) => {
+    slide.addEventListener("load", () => {
+        loader.style.display = "none";
+    });
+});
 
 controlsElements.forEach((control, index) => {
     control.addEventListener("click", () => {
@@ -33,3 +43,4 @@ controlsElements.forEach((control, index) => {
         nextSlide();
     });
 });
+
